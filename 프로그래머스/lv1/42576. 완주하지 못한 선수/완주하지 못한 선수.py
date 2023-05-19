7ef7592af9ae9efbe63555180d7d8524f7a801a4
choidaesig <72694569+choidaesig@users.noreply.github.com>
2023-05-19 13:08:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.35 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,10 @@
 def solution(participant, completion):
     answer = ''
-    if len(set(participant))!=len(set(completion)):
-        answer=set(participant)-set(completion)
-        answer=list(answer)
-        answer=answer[0]
-    else:
-        participant=sorted(participant)
-        completion=sorted(completion)
+    participant=sorted(participant)
+    completion=sorted(completion)
         
-        for index in range(len(completion)):
-            if completion[index] != participant[index]:
-                return participant[index]
-
-        
-    return answer
+    for index in range(len(completion)):
+        if completion[index] != participant[index]:
+            
+            return participant[index]        
+    return participant[-1]
