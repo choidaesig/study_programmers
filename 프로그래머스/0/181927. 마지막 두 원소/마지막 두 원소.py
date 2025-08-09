63bfff87c610cd2f033c62f6204ccd6803255e7d
choidaesig <72694569+choidaesig@users.noreply.github.com>
2025-08-09 18:19:57 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,8 @@
+def solution(num_list):
+    answer = []
+    num = len(num_list)
+    if num_list[num-1] > num_list[num-2]:
+        num_list.append(num_list[num-1]-num_list[num-2])
+    else:
+        num_list.append(num_list[num-1]*2)
+    return num_list
